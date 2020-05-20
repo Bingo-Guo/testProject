8aba474f8e9a7e49cc146a33f70fa2ba2bda172d
gwb41129 <gwb41129@ly.com>
2020-05-20 16:47:57 +0800

    test demo

---
+++ src.main.java.com.bingo.test/Demo01.java
@@ -0,0 +1,12 @@
+package src.main.java.com.bingo.test;
+
+/**
+ * @author bingo
+ * @date 2020/5/20
+ */
+public class Demo01 {
+
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
